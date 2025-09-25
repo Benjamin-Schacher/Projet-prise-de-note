e32b105793ac72e776c37d1fd89ab02d9c3eaa14
John Doe <jd@dawan.fr>
2025-09-25 10:06:43 +0200

    initial commit core projet spring boot

---
+++ back/MesNotes/src/main/java/com/dawan/MesNotes/MesNotesApplication.java
@@ -0,0 +1,13 @@
+package com.dawan.MesNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesNotesApplication.class, args);
+	}
+
+}
---
+++ back/MesNotes/src/test/java/com/dawan/MesNotes/MesNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawan.MesNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
