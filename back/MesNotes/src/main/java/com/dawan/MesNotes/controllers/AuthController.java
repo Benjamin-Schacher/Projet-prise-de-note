8a72610cf7cb835ab84ff420edaf4fb1747141dc
Jonatan Ns <jd@dawan.fr>
2025-09-25 17:25:15 +0200

    feat : Add authController -> login and register

---
+++ back/MesNotes/src/main/java/com/dawan/MesNotes/controllers/AuthController.java
@@ -0,0 +1,56 @@
+package com.dawan.MesNotes.controllers;
+
+import com.dawan.MesNotes.entities.User;
+import com.dawan.MesNotes.repositories.UserRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/inscription")
+    public ResponseEntity<?> showResgister(@Valid @RequestBody User user){
+        if(userRepository.findByUsername(user.getUsername()).isPresent()){
+            return ResponseEntity.badRequest().body("Ce nom d'utilisateur est déjà pris. Veuillez en choisir un autre.");
+        }
+        if(userRepository.findByEmail(user.getEmail()).isPresent()){
+            return ResponseEntity.badRequest().body("Cette email est déjà utilisé.");
+        }
+
+        userRepository.save(user);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(Map.of("message", "Inscription réussie", "user", user));
+    }
+
+    @PostMapping("/connexion")
+    public ResponseEntity<?> showLogin(@Valid @RequestBody User user) {
+        Optional<User> existingUser = userRepository.findByUsername(user.getUsername());
+
+        if (existingUser.isEmpty()) {
+            return ResponseEntity.badRequest().body("Nom d'utilisateur invalide");
+        }
+
+        User foundUser = existingUser.get();
+
+        if (!foundUser.getPassword().equals(user.getPassword())) {
+            return ResponseEntity.badRequest().body("Mot de passe incorrect");
+        }
+
+        return ResponseEntity.ok(Map.of(
+                "message", "Connexion réussie",
+                "user", foundUser
+        ));
+    }
+
+}
--- back/MesNotes/src/main/java/com/dawan/MesNotes/repositories/UserRepository.java
+++ back/MesNotes/src/main/java/com/dawan/MesNotes/repositories/UserRepository.java
@@ -4,6 +4,13 @@ import com.dawan.MesNotes.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public interface UserRepository extends JpaRepository<User, Long > {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean findByPassword(String password);
 }
